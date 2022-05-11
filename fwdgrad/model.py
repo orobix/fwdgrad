55269f852bf01ef1e04d1b3e049214fc849f1212
belerico_t <federico.belotti@orobix.com>
2022-05-11 10:10:11 +0200

    Add convnet

--- fwdgrad/model.py
+++ fwdgrad/model.py
@@ -1,6 +1,7 @@
 from typing import List, Optional
 
 import torch
+import torch.nn.functional as F
 from torch import nn
 
 
@@ -36,3 +37,39 @@ class NeuralNet(nn.Module):
             x = getattr(self, f"act{i}")(getattr(self, f"fc{i}")(x))
         x = self.out(x)
         return x
+
+
+class ConvNet(nn.Module):
+    def __init__(self, input_size: int = 1, output_size: int = 10):
+        """Standard Convolutional Network layers for the MNIST dataset.
+
+        Args:
+            input_size (int): input size of the model.
+            output_size (int): The number of output classes.
+
+        """
+        super(ConvNet, self).__init__()
+        self.conv1 = torch.nn.Conv2d(input_size, 64, kernel_size=3, padding=1)
+        self.conv2 = torch.nn.Conv2d(64, 64, kernel_size=3, padding=1)
+        self.conv3 = torch.nn.Conv2d(64, 64, kernel_size=3, padding=1)
+        self.conv4 = torch.nn.Conv2d(64, 64, kernel_size=3, padding=1)
+        self.fc1 = torch.nn.Linear(3136, 1024)
+        self.fc2 = torch.nn.Linear(1024, output_size)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2)
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+        x = F.max_pool2d(x, 2)
+        x = x.view(x.shape[0], -1)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+
+if __name__ == "__main__":
+    m = ConvNet(1, 10)
+    print(m(torch.rand(32, 1, 28, 28)))
--- mnist_fwdgrad.py
+++ mnist_fwdgrad.py
@@ -15,25 +15,32 @@ from fwdgrad.loss import functional_xent
 def train_model(cfg: DictConfig):
     USE_CUDA = torch.cuda.is_available()
     DEVICE = torch.device(f"cuda:{cfg.device_id}" if USE_CUDA else "cpu")
-    mnist = torchvision.datasets.MNIST(
-        "/tmp/data",
-        train=True,
-        download=True,
-        transform=torchvision.transforms.Compose(
-            [
-                torchvision.transforms.ToTensor(),
-                torchvision.transforms.Lambda(lambda x: torch.flatten(x)),
-            ]
-        ),
-    )
+    if "NeuralNet" in cfg.model._target_:
+        mnist = torchvision.datasets.MNIST(
+            "/tmp/data",
+            train=True,
+            download=True,
+            transform=torchvision.transforms.Compose(
+                [
+                    torchvision.transforms.ToTensor(),
+                    torchvision.transforms.Lambda(lambda x: torch.flatten(x)),
+                ]
+            ),
+        )
+        input_size = mnist.data.shape[1] * mnist.data.shape[2]
+    else:
+        mnist = torchvision.datasets.MNIST(
+            "/tmp/data",
+            train=True,
+            download=True,
+            transform=torchvision.transforms.Compose([torchvision.transforms.ToTensor()]),
+        )
+        input_size = 1
     train_loader = hydra.utils.instantiate(cfg.dataset, dataset=mnist)
 
-    input_size = mnist.data.shape[1] * mnist.data.shape[2]
     output_size = len(mnist.classes)
     with torch.no_grad():
-        model = hydra.utils.instantiate(
-            cfg.model, input_size=input_size, output_size=output_size
-        )
+        model = hydra.utils.instantiate(cfg.model, input_size=input_size, output_size=output_size)
         model.to(DEVICE)
         model.train()
 
@@ -61,16 +68,11 @@ def train_model(cfg: DictConfig):
 
                 # Forward gradient + parmeter update (SGD)
                 params = tuple(
-                    [
-                        p.sub_(cfg.optimization.learning_rate * jvp * v_params[i])
-                        for i, p in enumerate(params)
-                    ]
+                    [p.sub_(cfg.optimization.learning_rate * jvp * v_params[i]) for i, p in enumerate(params)]
                 )
             t1 = time.perf_counter()
             t_total += t1 - t0
-            print(
-                f"Epoch [{epoch+1}/{cfg.optimization.epochs}], Loss: {loss.item():.4f}, Time (s): {t1 - t0:.4f}"
-            )
+            print(f"Epoch [{epoch+1}/{cfg.optimization.epochs}], Loss: {loss.item():.4f}, Time (s): {t1 - t0:.4f}")
         print(f"Mean time: {t_total / cfg.optimization.epochs:.4f}")
 
 
